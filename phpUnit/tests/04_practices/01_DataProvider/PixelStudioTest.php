82b5ae940cef20e8cda347e15b2298b343d8eabe
hieu-mai <hieu-mai@cybozu.vn>
2023-10-03 14:41:05 +0700

    init project practice phpunit

---
+++ phpUnit/tests/04_practices/01_DataProvider/PixelStudioTest.php
@@ -0,0 +1,19 @@
+<?php
+declare(strict_types=1);
+
+require_once (__DIR__ . '/../../../vendor/autoload.php');
+require_once (__DIR__ . '/../../../src/04_practices/Constant.php');
+
+use exp\src\practice\PixelStudio;
+use PHPUnit\Framework\TestCase;
+
+class PixelStudioTest extends TestCase {
+    public function testGetSaleGamesReturnCorrectData() {
+        //TODO: Test function getSaleGames() will return correct data
+        //Require: Use data provider
+    }
+
+    public function testGetStudioName() {
+        //TODO: Test function getStudioName()
+    }
+}
